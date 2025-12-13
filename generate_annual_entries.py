d9c2340d36543147ce2e4bd96bd06ecce4616ca2
mucube <mucube0@gmail.com>
2025-12-12 18:53:41 -0700

    split data annually for climateaf

---
+++ generate_annual_entries.py
@@ -0,0 +1,13 @@
+# split data based on year to input into ClimateAF
+import pandas as pd
+
+df = pd.read_csv("generated_data.csv")
+
+df.insert(1, 'placeholder1', '.')
+df['placeholder2'] = '.'
+df.drop(columns=["Source", "YieldTonHa"], inplace=True)
+
+unique_years = df['HarvestYear'].unique()
+for year in unique_years:
+    annual_df = df[df['HarvestYear'] == year]
+    annual_df.to_csv(f'./annual_entries/{year}.csv')
