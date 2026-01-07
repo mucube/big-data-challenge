ead447b99908413c5f24d15abc82186f1e77d45f
mucube <mucube0@gmail.com>
2026-01-06 17:18:58 -0700

    Use HalvingRandomSearchCV instead of GridSearchCV for faster training

--- ml/rf_cv.py
+++ ml/rf_cv.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import HalvingRandomSearchCV, train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.utils import resample
 
 import pickle
 
-df = pd.read_csv("../training/final_data.csv")
+df = pd.read_csv("../training/final_data_filtered.csv")
 
 X = df[[
     "HarvestYear",
@@ -32,7 +32,7 @@ cv_params = {'max_depth': [None, 10, 20],
              'n_estimators': [300, 400, 500, 600]
              }
 
-rf_cv = GridSearchCV(rf, cv_params, scoring='r2', cv=5, n_jobs=4)
+rf_cv = HalvingRandomSearchCV(rf, cv_params, scoring='r2', cv=5, n_jobs=4, factor=3)
 
 rf_cv.fit(X_train,y_train)
 
