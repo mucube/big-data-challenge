9e6b1f97cb3ef2d7e868628f8b5529df24920f58
mucube <mucube0@gmail.com>
2026-01-06 18:14:11 -0700

    updated ml code

--- ml/rf_cv.py
+++ ml/rf_cv.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import HalvingRandomSearchCV, train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.utils import resample
+from sklearn.model_selection import KFold
+from sklearn.experimental import enable_halving_search_cv #needed
+from sklearn.model_selection import HalvingGridSearchCV
 
 import pickle
 
@@ -19,22 +19,24 @@ X = df[[
 
 y = df["YieldTonHa"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
 rf = RandomForestRegressor(random_state=0)
 
 cv_params = {'max_depth': [None, 10, 20], 
              'min_samples_leaf': [1,2,3],
              'min_samples_split': [2,5,10],
              'max_features': ["sqrt", "log2", None],
-             'n_estimators': [300, 400, 500, 600]
+             'n_estimators': [200, 300, 400]
              }
 
-rf_cv = HalvingRandomSearchCV(rf, cv_params, scoring='r2', cv=5, n_jobs=4, factor=3)
+cv = KFold(
+    n_splits=5,
+    shuffle=True,
+    random_state=42
+)
 
-rf_cv.fit(X_train,y_train)
+rf_cv = HalvingGridSearchCV(estimator=rf, param_grid=cv_params, scoring='r2', cv=cv, n_jobs=4, factor=3, verbose=1)
+
+rf_cv.fit(X,y)
 
 with open("object.pkl", "wb") as f:
     pickle.dump(rf_cv, f)
